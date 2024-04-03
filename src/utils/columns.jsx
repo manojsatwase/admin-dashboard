f6515ea26f9eb91f6753323a1d4425fd409c0d3b
manoj_s <apple@manojsatwase.local>
2024-04-03 09:43:27 +0530

    add columns for tables

---
+++ src/utils/columns.jsx
@@ -0,0 +1,22 @@
+export const columns = [
+    {
+        Header: "Id",
+        accessor: "_id",
+    },
+    {
+        Header: "Quantity",
+        accessor: "quantity",
+    },
+    {
+        Header: "Discount",
+        accessor: "discount",
+    },
+    {
+        Header: "Amount",
+        accessor: "amount",
+    },
+    {
+        Header: "Status",
+        accessor: "status",
+    },
+];
